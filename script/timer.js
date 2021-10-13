e27044c7bf941d2936f2c11215a00666707d7501
jinnnii <jinnnii@naver.com>
2021-10-13 22:57:21 +0900

    Refactoring Timer Module

--- script/timer.js
+++ script/timer.js
@@ -1,44 +1,65 @@
 import * as Result from './result.js';
-
-//start time out
 const playBtn = document.querySelector('.play-btn');
+const playView = document.querySelector('.play-view');
 const timeCounter = document.querySelector('.time-counter');
 
-const setTime = 3;
-let time = setTime;
-
-function timer() {
-    time--;
-    let min = parseInt(time / 60);
-    let sec = time % 60;
-    min = min >= 10 ? min : '0' + min;
-    sec = sec >= 10 ? sec : '0' + sec;
-    timeCounter.innerHTML = `${min}:${sec}`;
-
-    if (time < 0) {
-        time = setTime;
-        timeCounter.innerHTML = `00:00`;
-        playBtn.dispatchEvent(new Event('click'));
-        //window.alert("time out");
-        Result.lose();
+class Timer {
+    constructor(time) {
+        this.setTime = time;
+        this.time = this.setTime;
+        this.state = false; //stopping
     }
+
+    timer() {
+        this.time--;
+        let min = parseInt(this.time / 60);
+        let sec = this.time % 60;
+        min = min >= 10 ? min : '0' + min;
+        sec = sec >= 10 ? sec : '0' + sec;
+        timeCounter.innerHTML = `${min}:${sec}`;
+
+        if (this.time < 0) {
+            this.timerInit();
+            playBtn.dispatchEvent(new Event('click'));
+            Result.lose();
+        }
+    }
+
+    timerInit() {
+        this.time = this.setTime;
+        timeCounter.innerHTML = `00:00`;
+        this.state = false;
+    }
+
+    forceStart() {
+        playBtn.dispatchEvent(new Event('click'));
+    }
+
+    stateChange() {
+        if (this.state) { //true =playing
+            this.state = false;
+            playBtn.innerHTML = '<i class="fas fa-play"></i>'
+            this.timerInit();
+            Result.lose();
+
+        } else {  //false= stopping
+            this.state = true;
+            playBtn.innerHTML = '<i class="fas fa-stop"></i>';
+            playView.style.visibility = 'visible';
+        }
+    }
+
 }
 
-export default setInterval(() => {
-    if (playBtn.classList.contains('play')) {
-    } else if (playBtn.classList.contains('stop')) {
-        timer();
+export const timer = new Timer(30);
+
+setInterval(() => {
+    if (timer.state) {
+        timer.timer();
     }
 }, 1000);
 
 //start time out
 playBtn.addEventListener('click', () => {
-    playBtn.classList.toggle('stop');
-    playBtn.classList.toggle('play');
-
-    if (playBtn.classList.contains('play')) {
-        playBtn.innerHTML = '<i class="fas fa-play"></i>'
-    } else if (playBtn.classList.contains('stop')) {
-        playBtn.innerHTML = '<i class="fas fa-stop"></i>';
-    }
+    timer.stateChange();
 });
