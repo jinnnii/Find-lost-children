a993493d60b88588eadb6d0ad70fd69d3e057a50
jinnnii <jinnnii@naver.com>
2021-10-13 13:13:09 +0900

    Add Pos Class

---
+++ script/pos.js
@@ -0,0 +1,35 @@
+const playView = document.querySelector('.play-view');
+const etcMan = document.querySelector('.etc img');
+
+const playViewRect = playView.getBoundingClientRect();
+const manRect = etcMan.getBoundingClientRect();
+
+export default class Pos {
+    constructor() {
+        this.posX = 0;
+        this.posY = 0;
+        this.viewTop = playViewRect.top;
+        this.viewLeft = playViewRect.left;
+        this.viewWidth = playViewRect.width;
+        this.viewHeight = playViewRect.height;
+        this.manWidth = manRect.width / 2;
+        this.manHeight = manRect.height / 2;
+    }
+
+    RandomPos() {
+        this.posX = (Math.random() * this.viewWidth) - this.manWidth;
+        this.posY = (Math.random() * this.viewHeight) - this.manHeight;
+    }
+
+    setPos(target, posX, posY) {
+        if (posX < 0) posX = 0;
+        if (posX > this.viewWidth) posX = this.viewWidth;
+        if (posY < 0) posY = 0;
+        if (posY > this.viewHeight) posY = this.viewHeight;
+
+        target.style.transform = `translate(${posX}px,${posY}px)`;
+        target.style.zIndex = parseInt(posY);
+
+        return { x: posX, y: posY };
+    }
+}
